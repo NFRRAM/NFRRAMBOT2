4d8135cebd1c66aefafb77f68fcf794920537020
Ram :) <ramcruz999@gmail.com>
2024-07-27 07:21:33 +0800

    original code from erestain

---
+++ Files from old pc/data D/.Ram's Files/ClickUp and Erestain Bootcamp Stuff/erestain workers code/erestainOrigCode/src/commands.ts
@@ -0,0 +1,13 @@
+export const COMMANDS = {
+    name: "button",
+    description: "Creates a button",
+    options: [
+        /*
+            {
+                type: 1,
+                name: "option",
+                description: "button option"
+            }
+        */
+    ]
+}
